67434f6deff710602952e41f6a5a865493cde190
Domsmasher1 <47263379+Domsmasher1@users.noreply.github.com>
2023-05-28 18:56:53 +1000

    Update manage_form.php

--- Assignment part 2/manage_form.php
+++ Assignment part 2/manage_form.php
@@ -6,8 +6,8 @@
     <meta name="author" content="Daniel F">
     <meta name ="Viewport" content="width=device-width, initial scale=1.0">
     <link href="styles/styles.css" rel="stylesheet" media="screen and (max-width: 1920px)">
-    <title>Manage</title> 
-    <?php include 'header.inc' ?>
+    <title>Manage</title>
+    <?php include 'header.inc'?>
 </head>
 
 <body class="background">
@@ -22,24 +22,42 @@
 
             <h1>Select option to generate your report<h1><br>
 
-            <h3>          
+            <h3>
                 <label>Display all EOIs </label><input type="checkbox" name="manageselection" value="allEOI"><br>
 
-                <label>Specify a position to view any EOIs for it</label><input type="text" name="specifyposition" required pattern="{5}"><br>
+                <label>Job Reference Number </label>
+                <select name="jobRefNum" id="jobRefNum">
+                <?php
+                    foreach (getAllEOIs() as $option) {
+                        echo '<option value="' . $option . '">' . $option . '</option>';
+                    } ?>
+                </select><br>
 
-                <label>Specify a applicant to view their EOIs</label><input type="text" name="specifyapplicant" required pattern="[A-Za-z].{,20}"><br>
+                <label>What would you like to do with the EOI table</label><br>
+                    <label for="option1">View</label>
+                    <input type="radio" name="option" value="option1" id="option1" required>
 
-                <label>Specify a position to delete any EOIs for it  </label><input type="text" name="positiondeletion" required pattern="{5}"><br>
+                    <label for="option2">Delete</label>
+                    <input type="radio" name="option" value="option2" id="option2" required>
 
-                <label>Enter a Job Reference Number and select its new status:</label>
-                <input type="text" name="jobrefnum" required pattern="{5}"><br>
+                    <label for="option3">Update Status</label>
+                    <input type="radio" name="option" value="option3" id="option3" required><br>
 
-                <input type ="radio" name="stat" value="new" id="new" >
-                <label for="new">New</label><br>
-                <input type ="radio" name="stat"value="current" id="current" >
-                <label for="current">Current</label><br>s
-                <input type ="radio" name="stat" value="final" id="final" >
-                <label for="final">Final</label><br>
+                <label>Specify a applicant to view their EOIs (Only use if using View)</label><br>
+                <select name="option" id="option">
+                <?php
+                    foreach (getAllApplicats() as $option) {
+                        echo '<option value="' . $option . '">' . $option . '</option>';
+                    } ?>
+                </select><br>
+
+                <label>Status to update (Only use if using update status)<label><br>
+                <input type ="radio" value="new" id="new" name="status">
+                <label for="new">New </label>
+                <input type ="radio" value="current" id="current" name="status">
+                <label for="current">Current </label>
+                <input type ="radio" value="final" id="final" name="status">
+                <label for="final">Final</label>
 
                 <div class="wrap50 padding10">
                 <input type="submit" value="Submit" class="buttonGeneral">
@@ -47,7 +65,7 @@
             </h3>
         </form>
 
-        </div>      
+        </div>
 
         </article>
         <footer>
